8230331ef00d836b7c6e2545f27eab0cda0e697c
Alex Burns <jharopa@gmail.com>
2024-04-08 21:34:21 +0100

    ci/cd: GitHub Actions production deployment
    Updated docker-compose.yml to fix typo causing blank app folder to be created
    
    Issue: #3

--- bug_tracker/bug_tracker/settings.py
+++ bug_tracker/bug_tracker/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG", default=0))
+DEBUG = bool(os.getenv("DJANGO_DEBUG") == "True")
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
