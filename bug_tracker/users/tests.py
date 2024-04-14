26917f435949e0f4b28ddbc9d199627dca8f6ad2
Alex Burns <jharopa@gmail.com>
2024-04-14 16:08:20 +0100

    refactor: User types
    Updated tests breaking test cases into individual test methods
    
    Issue: #4

--- bug_tracker/users/tests.py
+++ bug_tracker/users/tests.py
@@ -5,51 +5,94 @@ from .models import CustomUser
 
 
 class CustomUserManagersTests(TestCase):
+    User = get_user_model()
 
-    def test_create_user(self):
-        User = get_user_model()
-        user: CustomUser = User.objects.create_user(
+    @classmethod
+    def setUpTestData(cls):
+        cls.User.objects.create_user(
             email="normal@test.com", first_name="John", last_name="Doe", password="test"
         )
 
-        self.assertEqual(user.email, "normal@test.com")
-        self.assertEqual(user.first_name, "John")
-        self.assertEqual(user.last_name, "Doe")
-        self.assertTrue(user.is_active)
-        self.assertFalse(user.is_superuser)
-
-        with self.assertRaises(AttributeError):
-            self.assertIsInstance(user.username)
-
-        with self.assertRaises(TypeError):
-            User.objects.create_user()
-
-        with self.assertRaises(ValueError):
-            User.objects.create_user(
-                email="", first_name="", last_name="", password="test"
-            )
-
-    def test_create_superuser(self):
-        User = get_user_model()
-        user: CustomUser = User.objects.create_superuser(
+        cls.User.objects.create_superuser(
             email="super@test.com", first_name="John", last_name="Doe", password="test"
         )
 
-        self.assertEqual(user.email, "super@test.com")
+    def test_create_user_email(self):
+        user: CustomUser = self.User.objects.get(pk=1)
+
+        self.assertEqual(user.email, "normal@test.com")
+
+    def test_create_user_first_name(self):
+        user: CustomUser = self.User.objects.get(pk=1)
+
         self.assertEqual(user.first_name, "John")
+
+    def test_create_user_last_name(self):
+        user: CustomUser = self.User.objects.get(pk=1)
+
         self.assertEqual(user.last_name, "Doe")
+
+    def test_create_user_is_active(self):
+        user: CustomUser = self.User.objects.get(pk=1)
+
         self.assertTrue(user.is_active)
+
+    def test_create_user_is_not_staff(self):
+        user: CustomUser = self.User.objects.get(pk=1)
+
+        self.assertFalse(user.is_staff)
+
+    def test_create_user_is_not_superuser(self):
+        user: CustomUser = self.User.objects.get(pk=1)
+
+        self.assertFalse(user.is_superuser)
+
+    def test_create_user_no_values_rasies_type_error(self):
+        with self.assertRaises(TypeError):
+            self.User.objects.create_user()
+
+    def test_create_user_empty_attributes_raises_value_error(self):
+        with self.assertRaises(ValueError):
+            self.User.objects.create_user(
+                email="", first_name="", last_name="", password="test"
+            )
+
+    def test_create_superuser_is_active(self):
+        user: CustomUser = self.User.objects.get(pk=2)
+
+        self.assertTrue(user.is_active)
+
+    def test_create_superuser_is_staff(self):
+        user: CustomUser = self.User.objects.get(pk=2)
+
         self.assertTrue(user.is_staff)
+
+    def test_create_superuser_is_superuser(self):
+        user: CustomUser = self.User.objects.get(pk=2)
+
         self.assertTrue(user.is_superuser)
 
 
 class CustomUserTests(TestCase):
-    def test_custom_user(self):
-        User = get_user_model()
-        user: CustomUser = User.objects.create_user(
+    User = get_user_model()
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.User.objects.create_user(
             email="normal@test.com", first_name="John", last_name="Doe", password="test"
         )
 
+    def test_custom_user_str(self):
+        user: CustomUser = self.User.objects.get(pk=1)
+
         self.assertEqual(str(user), "John Doe")
+
+    def test_custom_user_default_is_developer(self):
+        user: CustomUser = self.User.objects.get(pk=1)
+
         self.assertTrue(user.is_developer)
+
+    def test_custom_user_default_is_not_manager(self):
+        user: CustomUser = self.User.objects.get(pk=1)
+
         self.assertFalse(user.is_manager)
