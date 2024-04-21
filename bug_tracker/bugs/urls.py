b9ac75771e666941ce35608ffd1bc2d132783601
Alex Burns <jharopa@gmail.com>
2024-04-21 18:59:30 +0100

    user story: Bug report detail
    Added bug report detail view
    Added bug report detail template for use by the aforementioned view
    Added bug report detail url to bug app's urlconf
    Update bug list template to include link to detail pages
    
    Issue: #24

--- bug_tracker/bugs/urls.py
+++ bug_tracker/bugs/urls.py
@@ -8,6 +8,7 @@ app_name = "bugs"
 urlpatterns = [
     path("", views.BugListView.as_view(), name="bug_list"),
     path("create/", views.BugCreateView.as_view(), name="bug_create"),
+    path("detail/<int:id>/", views.BugDetailView.as_view(), name="bug_detail"),
     path("update/<int:id>/", views.BugUpdateView.as_view(), name="bug_update"),
     path("delete/<int:id>/", views.BugDeleteView.as_view(), name="bug_delete"),
 ]
