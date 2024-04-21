23bce481e829b486de366361cb499f538e7a3e54
Alex Burns <jharopa@gmail.com>
2024-04-21 01:11:49 +0100

    user story: Bug report update/delete/assign
    Added bug report update and delete views
    Added update and delete templates
    Added update and delete urls to bug urlconf
    Update bug list template to use update and detete features
    
    Issue: #7

--- bug_tracker/bugs/views.py
+++ bug_tracker/bugs/views.py
@@ -1,8 +1,9 @@
 from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 from users.models import CustomUser
 
 from .forms import BugFormDeveloper, BugFormManager
@@ -54,3 +55,45 @@ class BugListView(LoginRequiredMixin, ListView):
             context["is_manager"] = True
 
         return context
+
+
+class BugUpdateView(LoginRequiredMixin, UpdateView):
+    model = Bug
+    template_name = "bugs/update.html"
+    context_object_name = "bug"
+
+    login_url = settings.LOGIN_URL
+
+    def get_form_class(self):
+        if self.request.user.is_manager:
+            return BugFormManager
+        else:
+            return BugFormDeveloper
+
+    def get_object(self):
+        id = self.kwargs.get("id")
+        return get_object_or_404(Bug, id=id)
+
+    def get_success_url(self):
+        return reverse("bugs:bug_list")
+
+
+class BugDeleteView(LoginRequiredMixin, DeleteView):
+    model = Bug
+    template_name = "bugs/delete.html"
+    context_object_name = "bug"
+
+    login_url = settings.LOGIN_URL
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_developer:
+            return redirect("bugs:bug_list")
+
+        return super().get(request, *args, **kwargs)
+
+    def get_object(self):
+        id = self.kwargs.get("id")
+        return get_object_or_404(Bug, id=id)
+
+    def get_success_url(self):
+        return reverse("bugs:bug_list")
